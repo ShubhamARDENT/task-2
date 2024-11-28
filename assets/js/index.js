ac1ca35adb8f3daacacbe26f1f6e3ffaaca64c5c
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-11-28 18:48:34 +0530

    added js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,21 @@
 // Functionality of the page should appear here
+function getAscii(event) {
+  let key = event.key;
+
+  // only one key at time
+  if (key.length === 0) {
+    document.getElementById("display").textContent =
+      "key:  And the Ascii Value ";
+  } else if (key.length === 1) {
+    let Value = key.charCodeAt(0);
+    document.getElementById(
+      "display"
+    ).textContent = `Key:${key} and the Ascii Value: ${Value}`;
+  }
+}
+
+// clear btn
+function clearBtn() {
+  document.getElementById("display").textContent =
+    "key:  And the Ascii Value: ";
+}
