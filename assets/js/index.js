7a4470833e2eb3c41131c5f17bcc12335deb0848
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-11-29 18:24:41 +0530

    js code redone

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,48 @@
 // Functionality of the page should appear here
+
+const inputBox = document.getElementById("inputbox");
+const output = document.getElementById("output");
+const box = document.querySelector(".value-container").classList;
+const error = document.querySelector(".error-message");
+const errorBox = document.querySelector(".error-message").classList;
+
+inputBox.addEventListener("keydown", getAscii);
+
 function getAscii(event) {
   // get the key pressed
   let key = event.key;
 
   //shows on character from a-z and numbers in input box
   if (key.length === 1) {
-    document.getElementById("inputbox").value = key;
+    inputBox.value = key;
   }
 
   //check key length and show o/p on display
   if (key.length === 0) {
-    document.getElementById("display").textContent =
-      "key:  And the Ascii Value ";
+    output.textContent = "key: N/A  And the Ascii Value: N/A ";
   }
 
   if (key.length === 1) {
-    const Char = /^[a-zA-z0-9]/;
+    const Char = /^[a-zA-Z0-9]/;
     //check for character or number
     if (Char.test(key)) {
       let Value = key.charCodeAt(0);
-      document.getElementById(
-        "display"
-      ).textContent = `Key:${key} and the Ascii Value: ${Value}`;
+      output.textContent = `Key:${key} and the Ascii Value: ${Value}`;
+      box.remove("box");
+      // remove error msg
+      error.textContent = null;
     } else {
-      document.getElementById("display").textContent =
-        "please Enter a Alphabet Or Number Only";
+      output.textContent = "key: N/A  And the Ascii Value: N/A ";
+      box.add("box");
+      // show error msg
+      error.textContent = "Please Enter a Alphabet Or Number Only";
+      // blinking effect
+      errorBox.add("error-box");
     }
   }
 }
 
+document.querySelector(".clear-btn", clearBtn);
 // clear btn
 function clearBtn() {
   document.getElementById("display").textContent =
