20bcb9f5f344c89d459d545ae381703767aac8d7
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-11-29 12:43:11 +0530

    new js added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,30 @@
 // Functionality of the page should appear here
 function getAscii(event) {
+  // get the key pressed
   let key = event.key;
 
-  // only one key at time
+  //shows on character from a-z and numbers in input box
+  if (key.length === 1) {
+    document.getElementById("inputbox").value = key;
+  }
+
+  //check key length and show o/p on display
   if (key.length === 0) {
     document.getElementById("display").textContent =
       "key:  And the Ascii Value ";
-  } else if (key.length === 1) {
-    let Value = key.charCodeAt(0);
-    document.getElementById(
-      "display"
-    ).textContent = `Key:${key} and the Ascii Value: ${Value}`;
+  }
+
+  if (key.length === 1) {
+    const Char = /^[a-zA-Z0-9]$/;
+    if (Char.test(key)) {
+      let Value = key.charCodeAt(0);
+      document.getElementById(
+        "display"
+      ).textContent = `Key:${key} and the Ascii Value: ${Value}`;
+    } else {
+      document.getElementById("display").textContent =
+        "Error Invalid character";
+    }
   }
 }
 
